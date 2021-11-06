2456b98930acc9ff85db8e8baf9ed64718ddbc6d
AshutoshPatole <apatole479@gmail.com>
2021-11-06 20:22:17 +0530

    improved convertCase

--- convertCase.sh
+++ convertCase.sh
@@ -1,28 +1,48 @@
 #!/usr/bin/env bash
 
-read -p "Enter the file name : " filename
 
-if [[ ! -f "$filename" ]]; then
-  echo "File doesn't exists."
-  exit 1
-fi
-
-result=$(cat $filename)
-
-read -p "Enter the output file name : " outFile
-
-echo -e "***Choose one of the option***\n1. UpperCase\n2. LowerCase\n"
+echo -e "Choose an option\n1. String conversion\n2. File Conversion"
 
 read choice
 
 case $choice in
-  1 ) echo $result | tr "[[:lower:]]" "[[:upper:]]" >> $outFile
+  1 ) read -p "Enter the string : " string
+      echo -e "***Choose one of the option***\n1. UpperCase\n2. LowerCase\n"
+      read strCaseType
+      case $strCaseType in
+        1 ) echo $string | tr "[[:lower:]]" "[[:upper:]]"
+          ;;
+        2 ) echo $string | tr "[[:upper:]]" "[[:lower:]]"
+          ;;
+        * ) echo "Invalid option!"; exit 1
+          ;;
+      esac
     ;;
-  2 ) echo $result | tr "[[:upper:]]" "[[:lower:]]" >> $outFile
-    ;;
-  * ) echo "Invalid option!"; exit 1
-    ;;
+  2 ) read -p "Enter the file name : " filename
 
+  if [[ ! -f "$filename" ]]; then
+    echo "File doesn't exists."
+    exit 1
+  fi
+
+  result=$(cat $filename)
+
+  read -p "Enter the output file name : " outFile
+
+  echo -e "***Choose one of the option***\n1. UpperCase\n2. LowerCase\n"
+
+  read caseType
+
+  case $caseType in
+    1 ) echo $result | tr "[[:lower:]]" "[[:upper:]]" >> $outFile
+      ;;
+    2 ) echo $result | tr "[[:upper:]]" "[[:lower:]]" >> $outFile
+      ;;
+    * ) echo "Invalid option!"; exit 1
+      ;;
+
+  esac
+
+  echo "Done ."
+  ;;
 esac
-
-echo "Done ."
