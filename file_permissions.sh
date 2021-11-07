d93e090b22002d7d434d4c1aa4cdc9a5fb960268
AshutoshPatole <apatole479@gmail.com>
2021-11-07 18:40:43 +0530

    Add file permission listing

---
+++ file_permissions.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+function usage() {
+  cat <<- EOF
+usage : ./file_permission fileName
+fileName is required
+EOF
+}
+
+if [[ ! "$#" == 1 ]]; then
+  usage
+  exit 1
+fi
+
+if [[ ! -f "$1" ]]; then
+  echo "the file does not exists"
+  exit 1
+fi
+
+if [[ ! -r "$1" ]]; then
+  echo "the file does not have read permissions"
+fi
+
+if [[ ! -w "$1" ]]; then
+  echo "the file does not have write permissions"
+fi
+
+if [[ ! -x "$1" ]]; then
+  echo "the file does not have execute permissions"
+fi
+if [[ -w "$1" && -r "$1" && -x "$1" ]]; then
+  echo "the file has read, write and execute permissions"
+fi
