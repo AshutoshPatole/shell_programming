f98e15761d5c55982016e2f58a5f52c96138fabd
AshutoshPatole <apatole479@gmail.com>
2021-11-13 11:48:54 +0530

    update git commit file

--- README.md
+++ README.md
@@ -10,7 +10,7 @@ $ ./permission.sh
 This can be achieved by this command also
 
 ```
-$ chmod 744 *
+$ chmod 744 *.sh
 ```
 
 P.S: I don't know why I thought of creating a script for this 😅
--- git_commit.sh
+++ git_commit.sh
@@ -5,3 +5,8 @@ git pull origin master
 git add .
 git commit -m "$1"
 git push origin master
+
+# Add this script to the /usr/local/bin folder or create a symlink
+
+# sudo ln -s ~/path-to-script/git_commit.sh /usr/local/bin/gc.sh
+# and then use it anywhere like `gc.sh "Adding commit script to github"
